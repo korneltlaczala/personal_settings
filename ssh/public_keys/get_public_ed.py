823601e442642a0a2053ee15acf0a80c810f8732
Kornel <kornel.tlaczala@gmail.com>
2025-04-07 18:52:05 +0200

    add python scripts for quick public keys extraction

---
+++ ssh/public_keys/get_public_ed.py
@@ -0,0 +1,18 @@
+import os
+
+def find_pub_keys(directory):
+    keys = []
+    for file in os.listdir(directory):
+        if os.path.isdir(os.path.join(directory, file)):
+            keys.extend(find_pub_keys(os.path.join(directory, file)))
+        elif file.endswith(".pub"):
+            keys.append(os.path.join(directory, file))
+
+    return keys
+
+keys = find_pub_keys(".")
+keys = [key for key in keys if "ed" in key]
+with open("public_ed_keys.txt", "w") as f:
+    for key_file in keys:
+        with open(key_file, "r") as key:
+            f.write(key.read())
